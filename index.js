d0e1f6b4508cc59727ec6cd2d7d36a77e0f68de2
mercynjambi <njambim695@gmail.com>
2024-03-27 10:39:25 +0300

    return statements

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue -42); 
+  }
+  
+  function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264; 
+  }
+  
+  function distanceTravelledInFeet(start, stop) {
+    return Math.abs(start - stop) * 264; 
+  }
+  
+  function calculatesFarePrice(start, stop) {
+    const distance = distanceTravelledInFeet(start, stop);
+    let fare;
+  
+    if (distance <= 400) {
+      fare = 0;
+    } else if (distance > 400 && distance <= 2000) {
+      fare = (distance - 400) * 0.02; 
+    } else if (distance > 2000 && distance < 2500) {
+      fare = 25; 
+    } else {
+      return 'cannot travel that far'; 
+    }
+  
+    return fare;
+  }
+  
+  /*
+  module.exports = {
+    distanceFromHqInBlocks,
+    distanceFromHqInFeet,
+    distanceTravelledInFeet,
+    calculatesFarePrice
+  }; 
+  */
+
+  
